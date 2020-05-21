6e8247866e6a1d633d4732dadb5e81f002d49a09
milersant <63181336+milersant@users.noreply.github.com>
2020-05-21 07:44:51 -0600

    Add files via upload

---
+++ js/miler.js
@@ -0,0 +1,15 @@
+
+function validar(){
+
+var usuario = document.getElementById("usuario").value;
+var password = document.getElementById("contra").value;
+
+
+    if(usuario=="miler" && password=="12345")
+    {
+        alert("Bienvenido "+ usuario + " bonito dia")
+
+    } else{
+    alert("verifique sus datos")
+    }
+}
